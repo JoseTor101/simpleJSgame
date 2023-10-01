d479184abd997316bce7b1632d27057efa45550c
josetor101 <josetordecilla800@gmail.com>
2023-10-01 13:59:55 -0500

    Simple mines and doors game (First commit)

---
+++ findmines/game.js
@@ -0,0 +1,180 @@
+//https://platzi.com/new-home/clases/3573-javascript-practico-videojuegos/52327-arreglos-multidimensionales-en-javascript/
+
+const canvas = document.querySelector('#game');
+//Con cuantas dimensiones vamos a trabajar
+const game = canvas.getContext('2d');
+
+//game.&*()
+//Importante cargar todo el contenido antes
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+document.addEventListener('keydown', handleKeyDown);
+
+
+let canvasSize = Math.min(window.innerHeight*0.8,window.innerWidth*0.8)
+let elementSize = (canvasSize/10);
+let level = 0;
+let map = maps[level]
+let mapRowCols;
+let lives = 3;
+
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
+const giftPosition = {
+  x: undefined,
+  y: undefined,
+};
+
+let bombs = []
+
+const auxMap = {
+  x: undefined,
+  y: undefined,
+}
+
+function setCanvasSize(){
+    canvasSize = Math.min(window.innerHeight*0.8,window.innerWidth*0.8) 
+    elementSize = canvasSize/10;
+    
+    canvas.setAttribute('height',canvasSize);
+    canvas.setAttribute('width',canvasSize);
+    startGame()
+}
+
+
+function startGame(){
+
+    game.font = elementSize+'px Verdana';
+    game.textAlign= 'end';
+
+    map = maps[level];
+
+    console.log(lives)
+    if (level > 2){
+      gameWin();
+      return
+    }
+    const mapRows = map.trim().split('\n');
+    mapRowCols = mapRows.map( row => row.trim().split(''));
+    
+    game.clearRect(0,0,canvasSize, canvasSize);
+
+    mapRowCols.forEach((row,rowI) => {
+        row.forEach((col,colI)=>{
+            const emoji = emojis[col];
+            const posx = Math.floor(elementSize * (colI + 1));
+            const posy = Math.floor(elementSize * (rowI + 1));
+            game.fillText(emoji,posx,posy)
+            if(col == 'O'){
+              if (!playerPosition.x && !playerPosition.y) {
+                playerPosition.x  = posx;
+                playerPosition.y = posy;
+                auxMap.x = colI;
+                auxMap.y = rowI;
+              }
+            }
+        })
+    });
+    movePlayer();
+  }
+  
+function movePlayer(){
+  touchBomb();
+
+  if(mapRowCols[auxMap.y][auxMap.x] == 'I'){
+    console.log('Subes de nivel!')
+    level ++
+    startGame();
+  }
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
+}
+
+function touchBomb(){
+    if (lives > 0){
+      if (mapRowCols[auxMap.y][auxMap.x] == 'X'){ 
+      lives--  
+      playerPosition.x = undefined
+      playerPosition.y = undefined
+      startGame()
+    }
+    }
+    else {
+      console.log("Perdiste :(")
+      level = 0;
+      lives = 3;
+      playerPosition.x = undefined
+      playerPosition.y = undefined
+      gameOver()
+    }
+}
+
+function gameWin(){
+  console.log("Juego terminado")
+}
+
+function gameOver(){
+  console.log("Perdiste :(")
+  level = 0;
+  lives = 3;
+  startGame()
+}
+// Function to handle the keydown event
+function handleKeyDown(event) {
+  switch (event.key) {
+    case 'ArrowUp':
+      if (auxMap.y> 0 && auxMap.y <=9 ) {
+        playerPosition.y -= elementSize;
+        auxMap.y -= 1
+        startGame();
+      }
+      /*if (Math.floor(playerPosition.y)>elementSize) {
+        playerPosition.y -= elementSize;
+        auxMap.y -= 1
+        startGame();
+      }*/
+      break;
+    case 'ArrowDown':
+      if(auxMap.y>= 0 && auxMap.y <9 ) { 
+        playerPosition.y += elementSize;
+        auxMap.y += 1
+        startGame();
+      }
+      /*if(Math.ceil(playerPosition.y)<canvasSize) { 
+        playerPosition.y += elementSize;
+        auxMap.y += 1
+        startGame();
+      }*/
+      break;
+    case 'ArrowLeft':
+      if(auxMap.x> 0 && auxMap.x <=9 )  { 
+        playerPosition.x -= elementSize;
+        auxMap.x -= 1
+        startGame();
+      }
+      /*if (Math.floor(playerPosition.x)>elementSize) {
+        playerPosition.x -= elementSize;
+        auxMap.x -= 1
+        startGame();
+      }*/
+      break;
+    case 'ArrowRight':
+      if(auxMap.x>= 0 && auxMap.x <9 )  { 
+        playerPosition.x += elementSize;
+        auxMap.x += 1
+        startGame();
+      }
+      /*if(Math.ceil(playerPosition.x)<elementSize*10) { 
+        playerPosition.x += elementSize;
+        auxMap.x += 1
+        startGame();
+      }*/
+      break;
+    default:
+      break;
+  }
+}
+
+
---
+++ script.js
@@ -0,0 +1,5 @@
+aaaa.keydown(function (event) {
+    if (event.keyCode === 13) { 
+        //Event
+    }
+});
